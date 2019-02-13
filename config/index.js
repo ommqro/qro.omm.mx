832688251eac696bf9983c4514508301d38863e5
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-02-13 00:21:57 -0600

    Configuro nombres y demás

--- config/index.js
+++ config/index.js
@@ -1,23 +1,23 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
+  siteTitle: 'OMM Querétaro', // Navigation and Site Title
+  siteTitleAlt: 'OMM Querétaro - Olimpiada Nacional de Matemáticas para Alumnos de Primaria y Secundaria', // Alternative Site title for SEO
+  siteTitleManifest: 'OMMQueretaro',
+  siteUrl: 'https://querétaro.OMM.mx', // Domain of your site. No trailing slash!
+  siteLanguage: 'es', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Sitio oficial de OMM Querétaro, publicamos noticias, convocatorias y material de entrenamiento.', // Your site description
+  author: 'OMM Querétaro', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-6',
+  userTwitter: '@ommqro', // Twitter Username - Optional
+  // ogSiteName: 'minimal', // Facebook Site Name - Optional
+  // ogLanguage: 'en_US', // Facebook Language
+  // googleAnalyticsID: 'UA-47519312-6',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
