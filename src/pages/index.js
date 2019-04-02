mautematico/onmaps-queretaro-blog

Personalizo la página de inicio

src/pages/index.js
@@ -49,22 +49,22 @@ const IndexPage = ({
   <Layout>
     <Wrapper>
       <Hero>
-        <h1>Hi.</h1>
+        <h1>ONMAPS Querétaro</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          Avisos, convocatorias y material de entrenamiento para la Olimpiada Nacional de Matemáticas para Alumnos de
+          Primaria y Secundaria.
         </p>
         <Link to="/contact">
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
-            Contact
+            Contacto
           </Button>
         </Link>
       </Hero>
       <Content>
-        <SectionTitle>Latest stories</SectionTitle>
+        <SectionTitle>Avisos</SectionTitle>
         {postEdges.map(post => (
           <Article
             title={post.node.frontmatter.title}

ommqro/qro.omm.mx

src/pages/index.js
@@ -49,22 +49,22 @@ const IndexPage = ({
   <Layout>
     <Wrapper>
       <Hero>
-        <h1>Hi.</h1>
+        <h1>OMM Querétaro</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          Avisos, convocatorias y material de entrenamiento para la Olimpiada Nacional de Matemáticas para Alumnos de
+          Primaria y Secundaria.
         </p>
         <Link to="/contact">
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
-            Contact
+            Contacto
           </Button>
         </Link>
       </Hero>
       <Content>
-        <SectionTitle>Latest stories</SectionTitle>
+        <SectionTitle>Avisos</SectionTitle>
         {postEdges.map(post => (
           <Article
             title={post.node.frontmatter.title}