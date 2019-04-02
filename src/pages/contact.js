mautematico/onmaps-queretaro-blog

Personalizo la página de contacto

src/pages/contact.js
@@ -45,17 +45,24 @@ const Content = styled.div`
 const Contact = () => (
   <Layout>
     <Wrapper>
-      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Helmet title={`Contacto | ${config.siteTitle}`} />
       <Header>
         <Link to="/">{config.siteTitle}</Link>
       </Header>
       <Content>
-        <h1>Contact</h1>
-        <p>Super cool intro text to get people contacting me! It uses Netlify's form feature.</p>
+        <h1>Contacto</h1>
+        <p>
+          La mejor forma de contactarnos es por correo electrónico, escríbenos a{' '}
+          <a href="mailto:oemasqueretaro@hotmail.com">
+            <strong>oemasqueretaro@hotmail.com</strong>
+          </a>
+          .
+        </p>
+        <p>También, si lo prefieres, puedes llenar este formulario y nos pondremos en contacto contigo a la brevedad.</p>
         <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
           <p>
             <label htmlFor="contact-name">
-              Name
+              Nombre
               <input name="name" id="contact-name" type="text" required />
             </label>
           </p>
@@ -66,11 +73,11 @@ const Contact = () => (
           </p>
           <p>
             <label htmlFor="contact-message">
-              Your Message <textarea name="message" id="contact-message" required />
+              Tu mensaje <textarea name="message" id="contact-message" required />
             </label>
           </p>
           <p>
-            <Button>Send</Button>
+            <Button>Enviar</Button>
           </p>
           <input type="hidden" name="form-name" value="contact-form" />
         </form>

src/pages/success.js
@@ -43,13 +43,20 @@ const Content = styled.div`
 const Success = () => (
   <Layout>
     <Wrapper>
-      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Helmet title={`Contacto | ${config.siteTitle}`} />
       <Header>
         <Link to="/">{config.siteTitle}</Link>
       </Header>
       <Content>
-        <h1>Your message was sent!</h1>
-        <p>I'll answer your request as quick as possible.</p>
+        <h1>¡Tu mensaje fue enviado!</h1>
+        <p>Te responderemos tan pronto como sea posible.</p>
+        <p>
+          Anota nuestro correo electrónico, para futuras ocasiones:{' '}
+          <a href="mailto:oemasqueretaro@hotmail.com">
+            <strong>oemasqueretaro@hotmail.com</strong>
+          </a>
+          .
+        </p>
       </Content>
     </Wrapper>
   </Layout>

ommqro/qro.omm.mx

src/pages/contact.js
@@ -45,17 +45,24 @@ const Content = styled.div`
 const Contact = () => (
   <Layout>
     <Wrapper>
-      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Helmet title={`Contacto | ${config.siteTitle}`} />
       <Header>
         <Link to="/">{config.siteTitle}</Link>
       </Header>
       <Content>
-        <h1>Contact</h1>
-        <p>Super cool intro text to get people contacting me! It uses Netlify's form feature.</p>
+        <h1>Contacto</h1>
+        <p>
+          La mejor forma de contactarnos es por correo electrónico, escríbenos a{' '}
+          <a href="mailto:ommqro@hotmail.com">
+            <strong>ommqro@hotmail.com</strong>
+          </a>
+          .
+        </p>
+        <p>También, si lo prefieres, puedes llenar este formulario y nos pondremos en contacto contigo a la brevedad.</p>
         <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
           <p>
             <label htmlFor="contact-name">
-              Name
+              Nombre
               <input name="name" id="contact-name" type="text" required />
             </label>
           </p>
@@ -66,11 +73,11 @@ const Contact = () => (
           </p>
           <p>
             <label htmlFor="contact-message">
-              Your Message <textarea name="message" id="contact-message" required />
+              Tu mensaje <textarea name="message" id="contact-message" required />
             </label>
           </p>
           <p>
-            <Button>Send</Button>
+            <Button>Enviar</Button>
           </p>
           <input type="hidden" name="form-name" value="contact-form" />
         </form>

src/pages/success.js
@@ -43,13 +43,20 @@ const Content = styled.div`
 const Success = () => (
   <Layout>
     <Wrapper>
-      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Helmet title={`Contacto | ${config.siteTitle}`} />
       <Header>
         <Link to="/">{config.siteTitle}</Link>
       </Header>
       <Content>
-        <h1>Your message was sent!</h1>
-        <p>I'll answer your request as quick as possible.</p>
+        <h1>¡Tu mensaje fue enviado!</h1>
+        <p>Te responderemos tan pronto como sea posible.</p>
+        <p>
+          Anota nuestro correo electrónico, para futuras ocasiones:{' '}
+          <a href="mailto:ommqro@hotmail.com">
+            <strong>ommqro@hotmail.com</strong>
+          </a>
+          .
+        </p>
       </Content>
     </Wrapper>
   </Layout>